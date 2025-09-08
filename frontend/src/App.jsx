72c6c5b653b1c2de3fe79308aabd3cac372e4d3d
ReRun007 <117578115+ReRun007@users.noreply.github.com>
2025-09-08 19:00:29 +0700

    เพิ่ม rules ของ inline และปรับ config พื้นฐาน

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "@/pages/Home";
+import Login from "@/pages/auth/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         {/* เพิ่มหน้าใหม่ได้ที่นี่ */}
       </Routes>
     </Router>
